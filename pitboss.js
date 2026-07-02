04d852dbe5eeecb48ff989c20683c5e0df7d38da
gachaduar-ctrl <gachaduar@gmail.com>
2026-07-02 11:46:06 +0700

    Create pitboss.js

---
+++ pitboss.js
@@ -0,0 +1,19 @@
+{
+  "name": "Pitboss Schedule",
+  "type": "pitboss",
+  "sound": "siren",
+  "alarms": [
+    { "label": "Elan Plateau (Lv45)", "hour": 0, "minute": 0 },
+    { "label": "Elan Plateau (Lv45)", "hour": 6, "minute": 0 },
+    { "label": "Elan Plateau (Lv45)", "hour": 12, "minute": 0 },
+    { "label": "Elan Plateau (Lv45)", "hour": 18, "minute": 0 },
+    { "label": "Draco Desert (Lv50)", "hour": 2, "minute": 0 },
+    { "label": "Draco Desert (Lv50)", "hour": 8, "minute": 0 },
+    { "label": "Draco Desert (Lv50)", "hour": 14, "minute": 0 },
+    { "label": "Draco Desert (Lv50)", "hour": 20, "minute": 0 },
+    { "label": "Caliana (Lv55)", "hour": 4, "minute": 0 },
+    { "label": "Caliana (Lv55)", "hour": 10, "minute": 0 },
+    { "label": "Caliana (Lv55)", "hour": 16, "minute": 0 },
+    { "label": "Caliana (Lv55)", "hour": 22, "minute": 0 }
+  ]
+}
